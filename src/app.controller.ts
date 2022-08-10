01f5278f23d1d580f4e1348f8c444086dfd67e82
mayur.wakikar <mayurwakikar17@gmail.com>
2022-08-10 12:14:31 +0530

    added enters for sp

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,47 +2,9 @@ import { Controller, Delete, Get, Param, Patch, Post, Req, Request, } from '@nes
 //import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller('/:user')
+@Controller()
 export class AppController {
   //constructor(private readonly appService: AppService) { }
 
-  @Get()                                                   //base route with get method
-  // getHello(): string {                            //get is for getting the data     
-  //when you put /user it will show user controller
-  //   return this.appService.getHello();
-  // }
-  getUsers() {
-    //return "I am user";
-    return { name: "Abc", email: "abc@gmail.com" };
-  }
 
-  @Post()   //post method means returning somekind of data
-  store(@Req() req: Request) {              //use @Req() for request  ,, req as request and its type :request
-    //return "I am storing the data";           //you have to go /3000 and use post method it will show data
-    return req.body;
-  }
-
-  @Patch('/:userId')    //for updating  
-  update(@Req() req: Request) {
-    return req.body;
-  }
-
-
-
-
-
-
-  @Get('/:userId')    //for getting  the user
-  getUser(@Param() params: { userId: Number }) {
-    return params;
-  }
-
-
-
-
-
-  @Delete('/:userId')  //for deleting the index
-  deleteUser(@Param() params: { userId: Number }) {
-    return params;
-  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,49 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Patch, Post, Req, Request } from "@nestjs/common";    //also we have to import it 
 
 @Controller('/user')
 
-export class UserController {
-    @Get()
-    getUser() {
-        return "I am From User Controller";
+export class UserController {            //when we create a user we have to export a class & above it give controller
+    @Get()                                                   //base route with get method
+    // getHello(): string {                            //get is for getting the data     
+    //when you put /user it will show user controller
+    //   return this.appService.getHello();
+    // }
+    getUsers() {
+        //return "I am user";
+        return { name: "Abc", email: "abc@gmail.com" };
     }
-    @Post()
-    storedata() {
-        return "I stored user data";
+
+    @Post()   //post method means returning somekind of data
+    store(@Req() req: Request) {              //use @Req() for request  ,, req as request and its type :request
+        //return "I am storing the data";           //you have to go /3000 and use post method it will show data
+        return req.body;
+    }
+
+    @Patch('/:userId')    //for updating  
+    update(@Req() req: Request) {
+        return req.body;
+    }
+
+
+
+
+
+
+
+
+
+
+    @Get('/:userId')    //for getting  the user
+    getUser(@Param() params: { userId: Number }) {
+        return params;
+    }
+
+
+
+
+
+    @Delete('/:userId')  //for deleting the index
+    deleteUser(@Param() params: { userId: Number }) {
+        return params;
     }
 }
